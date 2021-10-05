c761a8db71fa0b0975338c2ee1c9cba9ac6bfddf
eloyba <51505594+eloyba@users.noreply.github.com>
2021-10-05 20:57:45 +0200

    Delete operar_variables2.py

--- operar_variables2.py
+++
@@ -1,8 +0,0 @@
-#numero1 = int(input("Primer numero a sumar: "))
-#numero2 = int(input("Segundo numero a sumar: "))
-
-numero1 = input("Primer numero a sumar: ")
-numero2 = input("Segundo numero a sumar: ")
-numero1 = int(numero1)
-numero2 = int(numero2)
-print (numero1 + numero2)
