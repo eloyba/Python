7d7677e98975d0e8aaf246192718903ae02c9a6f
eloyba <51505594+eloyba@users.noreply.github.com>
2021-10-05 20:56:50 +0200

    Delete condicional_elseif.py

--- condicional_elseif.py
+++
@@ -1,9 +0,0 @@
-numero = input("Elige un numero del 0 al 2: ")
-if numero == "0":
-    print("Has elegido el numero 0")
-elif numero == "1":
-    print("Has elegido el numero 1")
-elif numero == "2":
-    print("Has elegido el numero 2")
-else:
-    print("El numero elegido no esta entre el 0 y el 2 o no es un numero")
